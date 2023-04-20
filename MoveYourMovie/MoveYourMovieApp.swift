377b54d686b6e0a6e9e3d3bec79f4fd3efe0a98e
Vlad Kozlov <vlad.kozlov@idapgroup.com>
2023-04-20 16:18:36 +0300

    Initial Commit

---
+++ MoveYourMovie/MoveYourMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveYourMovieApp.swift
+//  MoveYourMovie
+//
+//  Created by Vlad Kozlov on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoveYourMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
